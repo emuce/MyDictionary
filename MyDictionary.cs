4d2ecfe2fca9d73e4ebea947bb7e7cff04b55f8d
emuce <eemuce@gmail.com>
2021-01-24 23:46:46 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] kArray;
+        TValue[] vArray;
+        TKey[] tempkArray;
+        TValue[] tempvArray;
+
+        public MyDictionary()
+        {
+            kArray = new TKey[0];
+            vArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempkArray = kArray;
+            tempvArray = vArray;
+            kArray = new TKey[kArray.Length + 1];
+            vArray = new TValue[vArray.Length + 1];
+
+            // array'in uzunluğu arttırıldıktan sonra geçici array'lerdeki elemanları ana array'lerimize aktarıyoruz.
+            for (int i = 0; i < tempkArray.Length; i++)
+            {
+                kArray[i] = tempkArray[i];
+            }
+
+            for (int i = 0; i < tempvArray.Length; i++)
+            {
+                vArray[i] = tempvArray[i];
+            }
+
+            // array^leri eşitleme işlemi bittikten sonra yeni elemanlarımızı ekliyoruz.
+            kArray[kArray.Length - 1] = key;
+            vArray[vArray.Length - 1] = value;
+        }
+
+        // iki metot listelerimizi consolda göstermemizi sağlar.
+        public TKey[] Key
+        {
+            get { return kArray; }
+        }
+
+        public TValue[] Value
+        {
+            get { return vArray; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<byte, string> plakaSehir = new MyDictionary<byte, string>();
+            plakaSehir.Add(67, "Zonguldak");
+            plakaSehir.Add(41, "Kocaeli");
+            plakaSehir.Add(21,"Diyarbakır");
+            plakaSehir.Add(64,"Uşak");
+            plakaSehir.Add(78,"Karabük");
+
+            Console.WriteLine("Plaka No : " + plakaSehir.Key[0] + "\tŞehir : " + plakaSehir.Value[0]);
+            Console.WriteLine("Plaka No : " + plakaSehir.Key[1] + "\tŞehir : " + plakaSehir.Value[1]);
+            Console.WriteLine("Plaka No : " + plakaSehir.Key[2] + "\tŞehir : " + plakaSehir.Value[2]);
+            Console.WriteLine("Plaka No : " + plakaSehir.Key[3] + "\tŞehir : " + plakaSehir.Value[3]);
+            Console.WriteLine("Plaka No : " + plakaSehir.Key[4] + "\tŞehir : " + plakaSehir.Value[4]);
+
+        }
+    }
+}
